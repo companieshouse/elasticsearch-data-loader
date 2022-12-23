bed0755ab41a7f73d5387ef0e2ef3085dc6aa7b7
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2022-12-23 14:11:41 +0000

    GCI-2211: Complete unit testing of getAlphaKeys().

--- companybindex/main_test.go
+++ companybindex/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/companieshouse/elasticsearch-data-loader/datastructures"
 	"github.com/companieshouse/elasticsearch-data-loader/eshttp"
@@ -41,7 +42,7 @@ func TestGetAlphaKeys(t *testing.T) {
 
 	Convey("Should handle failure to marshal company names by exiting program", t, func() {
 
-		// Save current function and restore at the end.
+		// Save real functions so that we restore them at the end.
 		realMarshal := marshal
 		defer func() { marshal = realMarshal }()
 		marshal = func(v interface{}) ([]byte, error) {
@@ -56,7 +57,7 @@ func TestGetAlphaKeys(t *testing.T) {
 		fatalf = func(format string, v ...interface{}) {
 			errorMessage := fmt.Sprintf(format, v)
 			// We replace os.Exit() with panic() because it too exits execution at the right point,
-			// but the GoConvey test framework can detect the latter.
+			// but the GoConvey test framework can detect the latter only.
 			panic(errorMessage)
 		}
 
@@ -74,6 +75,89 @@ func TestGetAlphaKeys(t *testing.T) {
 		So(func() { getAlphaKeys(transformer, &companies, 0, client) },
 			ShouldPanicWith,
 			"error marshal to json: [json: unsupported value: Test generated error]")
+
+	})
+
+	Convey("Should handle failure to get alpha keys by exiting program", t, func() {
+
+		// Save real functions so that we restore them at the end.
+		realFatalf := fatalf
+		defer func() { fatalf = realFatalf }()
+		fatalf = func(format string, v ...interface{}) {
+			errorMessage := fmt.Sprintf(format, v)
+			// We replace os.Exit() with panic() because it too exits execution at the right point,
+			// but the GoConvey test framework can detect the latter only.
+			panic(errorMessage)
+		}
+
+		unmarshalCalled := false
+		realUnmarshal := unmarshal
+		defer func() { unmarshal = realUnmarshal }()
+		unmarshal = func(data []byte, v interface{}) error {
+			unmarshalCalled = true
+			return nil
+		}
+
+		ctrl := gomock.NewController(t)
+		transformer := transform.NewMockTransformer(ctrl)
+		client := eshttp.NewMockClient(ctrl)
+		companies := []*datastructures.MongoCompany{{}}
+
+		companyNames := []datastructures.CompanyName{{Name: "@"}}
+		companyNamesBody, _ := json.Marshal(companyNames)
+
+		transformer.EXPECT().GetCompanyNames(&companies, 0).Return(companyNames)
+		client.EXPECT().GetAlphaKeys(companyNamesBody, alphakeyURL).Return(
+			nil, errors.New("Test generated error"))
+
+		So(func() { getAlphaKeys(transformer, &companies, 0, client) },
+			ShouldPanicWith,
+			"error fetching alpha keys: [Test generated error]")
+		So(unmarshalCalled, ShouldBeFalse)
+
+	})
+
+	Convey("Should handle failure to unmarshal company names by exiting program", t, func() {
+
+		// Save real functions so that we restore them at the end.
+		realUnmarshal := unmarshal
+		defer func() { unmarshal = realUnmarshal }()
+		unmarshal = func(data []byte, v interface{}) error {
+			return &json.UnmarshalTypeError{
+				Value:  "Test generated error",
+				Type:   reflect.TypeOf(""),
+				Offset: 0,
+				Struct: "struct",
+				Field:  "field",
+			}
+		}
+
+		realFatalf := fatalf
+		defer func() { fatalf = realFatalf }()
+		fatalf = func(format string, v ...interface{}) {
+			errorMessage := fmt.Sprintf(format, v)
+			// We replace os.Exit() with panic() because it too exits execution at the right point,
+			// but the GoConvey test framework can detect the latter only.
+			panic(errorMessage)
+		}
+
+		ctrl := gomock.NewController(t)
+		transformer := transform.NewMockTransformer(ctrl)
+		client := eshttp.NewMockClient(ctrl)
+		companies := []*datastructures.MongoCompany{{}}
+
+		companyNames := []datastructures.CompanyName{{Name: "@"}}
+		companyNamesBody, _ := json.Marshal(companyNames)
+
+		transformer.EXPECT().GetCompanyNames(&companies, 0).Return(companyNames)
+		client.EXPECT().GetAlphaKeys(companyNamesBody, alphakeyURL).Return(
+			[]byte("[{\"sameAsAlphaKey\":\"true\", \"orderedAlphaKey\":\"blah\"}]"), nil)
+
+		So(func() { getAlphaKeys(transformer, &companies, 0, client) },
+			ShouldPanicWith,
+			"error [json: cannot unmarshal Test generated error into Go struct field "+
+				"struct.field of type string [91 123 34 110 97 109 101 34 58 34 64 34 125 93]] "+
+				"unmarshalling alphakey response for %!s(MISSING)")
 
 	})
 
